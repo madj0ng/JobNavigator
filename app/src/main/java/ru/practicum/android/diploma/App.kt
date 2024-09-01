6cd4531ab4cfc8f40f373e637293eeeaf03a5674
vochik <vochik@live.ru>
2024-09-01 22:12:50 +0300

    добавил немного кода в класс, чтобы детект не ругался

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -3,4 +3,9 @@ package ru.practicum.android.diploma
 import android.app.Application
 
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        //Здесь будет код для коин модуля
+    }
 }
