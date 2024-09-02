9692cabde0dfbdd3f04dc82fbdb2cde779d0934e
madj0ng <madj0ng@yandex.ru>
2024-09-02 16:35:19 +0300

    Change abstract class AppDatabase

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
@@ -5,8 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import ru.practicum.android.diploma.data.db.dao.JobDao
+import ru.practicum.android.diploma.data.db.entity.JobEntity
 
-@Database(version = 1)
+@Database(
+    version = 1,
+    entities = [JobEntity::class]
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun jobDao(): JobDao
