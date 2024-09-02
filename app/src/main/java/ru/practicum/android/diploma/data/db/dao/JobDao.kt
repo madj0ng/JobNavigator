ca279215dff0801bc5dca0548c6dd885b39a6ddb
madj0ng <madj0ng@yandex.ru>
2024-09-02 14:43:18 +0300

    Change method interface JobDao to suspend

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/JobDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/JobDao.kt
@@ -10,14 +10,14 @@ import ru.practicum.android.diploma.data.db.entity.JobEntity
 @Dao
 interface JobDao {
     @Insert(entity = JobEntity::class, onConflict = OnConflictStrategy.REPLACE)
-    fun insertJob(jobEntity: JobEntity)
+    suspend fun insertJob(jobEntity: JobEntity)
 
     @Delete(entity = JobEntity::class)
-    fun deleteJob(jobEntity: JobEntity)
+    suspend fun deleteJob(jobEntity: JobEntity)
 
     @Query("SELECT * FROM job_table WHERE vacancyId = :vacancyId")
-    fun getJob(vacancyId: String): JobEntity
+    suspend fun getJob(vacancyId: String): JobEntity
 
     @Query("SELECT * FROM job_table")
-    fun getJobs(): List<JobEntity>
+    suspend fun getJobs(): List<JobEntity>
 }
