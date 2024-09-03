b3c2fcc3dfd21b7335e1d3aefb2f43c1a6380ade
vochik <vochik@live.ru>
2024-09-03 16:24:23 +0300

    Создан класс для проверки интернет соединения и инициализирован в соответствующем модуле

--- app/src/main/java/ru/practicum/android/diploma/di/UtillModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/UtillModule.kt
@@ -1,7 +1,13 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.util.IsConnected
 
 val utillModule = module {
-    // Здесь будут зависимости для утил слоя
+
+    factory {
+        IsConnected(androidContext())
+    }
+
 }
