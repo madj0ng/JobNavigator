e9222e7c2ebf0f0579122b7dbbb875b0b48e77ff
vochik <vochik@live.ru>
2024-09-03 16:34:01 +0300

    Исправлен нейминг класса и уменьшено колличество точек return в функции

--- app/src/main/java/ru/practicum/android/diploma/di/UtillModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/UtillModule.kt
@@ -2,12 +2,12 @@ package ru.practicum.android.diploma.di
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
-import ru.practicum.android.diploma.util.IsConnected
+import ru.practicum.android.diploma.util.Connected
 
 val utillModule = module {
 
     factory {
-        IsConnected(androidContext())
+        Connected(androidContext())
     }
 
 }
--- app/src/main/java/ru/practicum/android/diploma/util/Connected.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Connected.kt
@@ -4,19 +4,20 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
-class IsConnected(private val context: Context) {
+class Connected(private val context: Context) {
 
     fun isConnected(): Boolean {
+        var connectionStatus = false
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         if (capabilities != null) {
             when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> connectionStatus = true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> connectionStatus = true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> connectionStatus = true
             }
         }
-        return false
+        return connectionStatus
     }
 }
 
