115a96e46ce1387b17b82b4f3582bae5c7bf452e
vochikk <vochik@live.ru>
2024-09-12 01:09:11 +0300

    убрал неиспользуемые импорты

--- app/src/main/java/ru/practicum/android/diploma/data/network/NetworkExeption.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/NetworkExeption.kt
@@ -1,5 +1,4 @@
 package ru.practicum.android.diploma.data.network
 
-import retrofit2.HttpException
 
 class NetworkExeption(message: String, throwable: Throwable? = null) : Exception(message, throwable)
