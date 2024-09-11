176ce885eb64b3330f9245d357a4ec654fefa01a
vochikk <vochik@live.ru>
2024-09-12 01:42:00 +0300

    исправлена функция поиска и ее вызов в ретрофите

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -4,18 +4,18 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import ru.practicum.android.diploma.data.network.HhApi
+import ru.practicum.android.diploma.data.network.HHApiService
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
 val dataModule = module {
 
-    single<HhApi> {
+    single<HHApiService> {
         Retrofit.Builder()
             .baseUrl("https://api.hh.ru")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(HhApi::class.java)
+            .create(HHApiService::class.java)
     }
 
     single<NetworkClient> {
