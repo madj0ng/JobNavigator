7f6a9ad886b547846f2b286d06bb82a131c73acd
madj0ng <madj0ng@yandex.ru>
2024-09-12 11:51:30 +0300

    Create class JobSearchViewModel

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/JobSearchViewModel.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.presentation.models.JobSearchScreenState
+import ru.practicum.android.diploma.util.SingleLiveEvent
+
+class JobSearchViewModel : ViewModel() {
+
+    private val toastLiveData = SingleLiveEvent<String>()
+    private val screenLiveData = MutableLiveData<JobSearchScreenState>(JobSearchScreenState.Empty)
+}
