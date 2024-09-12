52b7b69abb75f368d1584bac9d89fd80b39072e6
madj0ng <madj0ng@yandex.ru>
2024-09-12 13:55:07 +0300

    Inicialization ViewModel

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.presentation.view_model.JobSearchViewModel
 
 val viewModelModule = module {
-    // Здесь будут зависимости для виев моделей
+
+    viewModel {
+        JobSearchViewModel()
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/jobsearch/JobSearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/jobsearch/JobSearchFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentJobSearchBinding
+import ru.practicum.android.diploma.presentation.view_model.JobSearchViewModel
 
 class JobSearchFragment : Fragment() {
     private var _binding: FragmentJobSearchBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: JobSearchViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
