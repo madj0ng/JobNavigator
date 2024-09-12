6aafda181061c3ec44c9f24ae89540f16d8d384d
madj0ng <madj0ng@yandex.ru>
2024-09-12 14:26:42 +0300

    Change name from viewModel to viewmodel

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.presentation.viewModel.JobSearchViewModel
+import ru.practicum.android.diploma.presentation.viewmodel.JobSearchViewModel
 
 val viewModelModule = module {
 
--- app/src/main/java/ru/practicum/android/diploma/presentation/viewmodel/JobSearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/viewmodel/JobSearchViewModel.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.presentation.viewModel
+package ru.practicum.android.diploma.presentation.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
