1d747762afb86ca52a0602b41d902918255914ac
madj0ng <madj0ng@yandex.ru>
2024-09-19 00:56:33 +0300

    Fix detect in RetrofitNetworkClient

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.withContext
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.data.dto.NetworkResponse
 import ru.practicum.android.diploma.data.dto.VacancySearchRequest
+import ru.practicum.android.diploma.data.dto.VacancySearchResponse
 import ru.practicum.android.diploma.data.dto.vacancydetail.VacancyDetailsRequest
 import ru.practicum.android.diploma.data.dto.vacancydetail.VacancyDetailsResponse
 import ru.practicum.android.diploma.util.Connected
@@ -29,40 +30,39 @@ class RetrofitNetworkClient(
             if (connected.isConnected()) {
                 try {
                     when (dto) {
-                        is VacancySearchRequest -> {
-                            val response = hhApi.getVacancies(
-                                dto.queryOptions
-                            )
-                            response.apply { resultCode = RESULT_CODE_SUCCESS }
-                        }
+                        is VacancySearchRequest -> vacancySearchRequest(dto)
 
-                        is VacancyDetailsRequest -> {
-                            val responseDto = hhApi.getVacancy(dto.vacancyId)
-                            val response = VacancyDetailsResponse(responseDto)
-                            response.apply { resultCode = RESULT_CODE_SUCCESS }
-                        }
+                        is VacancyDetailsRequest -> vacancyDetailsRequest(dto)
 
                         else -> {
-                            NetworkResponse().apply {
-                                resultCode = ERROR_CODE_SERVER
-                                message = context.getString(R.string.search_error_server)
-                            }
+                            errorResponse(ERROR_CODE_SERVER, R.string.search_error_server)
                         }
                     }
                 } catch (e: IOException) {
-                    NetworkResponse().apply {
-                        resultCode = ERROR_CODE_SERVER
-                        message = context.getString(R.string.search_error_server)
-                    }
+                    errorResponse(ERROR_CODE_SERVER, R.string.search_error_server)
                     throw e
                 }
             } else {
-                NetworkResponse().apply {
-                    resultCode = ERROR_CODE_INTERNET
-                    message = context.getString(R.string.search_error_no_connect)
-                }
+                errorResponse(ERROR_CODE_INTERNET, R.string.search_error_no_connect)
             }
 
+        }
+    }
+
+    private suspend fun vacancySearchRequest(vacancySearchRequest: VacancySearchRequest): VacancySearchResponse {
+        return hhApi.getVacancies(vacancySearchRequest.queryOptions).apply { resultCode = RESULT_CODE_SUCCESS }
+    }
+
+    private suspend fun vacancyDetailsRequest(vacancyDetailsRequest: VacancyDetailsRequest): NetworkResponse {
+        return VacancyDetailsResponse(hhApi.getVacancy(vacancyDetailsRequest.vacancyId)).apply {
+            resultCode = RESULT_CODE_SUCCESS
+        }
+    }
+
+    private fun errorResponse(errCode: Int, mesInt: Int): NetworkResponse {
+        return NetworkResponse().apply {
+            resultCode = ERROR_CODE_SERVER
+            message = context.getString(mesInt)
         }
     }
 
