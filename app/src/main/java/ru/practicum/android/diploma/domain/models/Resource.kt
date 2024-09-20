96899b8a3435d6b03227f208d175b2850da0b0a1
vochik <vochik@live.ru>
2024-09-20 19:36:09 +0300

    fix

--- app/src/main/java/ru/practicum/android/diploma/domain/models/Resource.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Resource.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.domain.models
 
 sealed interface Resource<T> {
-    data class Success<T>(val data: T, val found: Int? = null, val page: Int?, val pages: Int?) : Resource<T>
+    data class Success<T>(val data: T, val found: Int? = null, val page: Int? = null, val pages: Int? = null) : Resource<T>
     data class Error<T>(val resultCode: Int, val message: String) : Resource<T>
 }
