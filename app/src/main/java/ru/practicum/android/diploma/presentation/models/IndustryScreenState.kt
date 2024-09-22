7528cd5415bc867153ffe4044bcec0a04a5faa1a
vochik <vochik@live.ru>
2024-09-22 19:26:13 +0300

    убран вылет при отсутствии интернета на экране выбора отрасли (поправлен детект)

--- app/src/main/java/ru/practicum/android/diploma/presentation/models/IndustryScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/models/IndustryScreenState.kt
@@ -3,9 +3,9 @@ package ru.practicum.android.diploma.presentation.models
 import ru.practicum.android.diploma.domain.models.IndustryModel
 
 sealed interface IndustryScreenState {
-    data class Content (val data: List<IndustryModel>): IndustryScreenState
+    data class Content(val data: List<IndustryModel>) : IndustryScreenState
 
-    data object ErrorInternet: IndustryScreenState
+    data object ErrorInternet : IndustryScreenState
 
-    data object ErrorContent: IndustryScreenState
+    data object ErrorContent : IndustryScreenState
 }
