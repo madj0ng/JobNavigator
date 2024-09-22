8ccd596072bb1c3cbe2a2427b316094c7436672a
vochik <vochik@live.ru>
2024-09-22 18:38:31 +0300

    убран вылет при отсутствии интернета на экране выбора страны и региона (поправлен детект)

--- app/src/main/java/ru/practicum/android/diploma/data/network/ListResponseWrapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/ListResponseWrapper.kt
@@ -2,4 +2,4 @@ package ru.practicum.android.diploma.data.network
 
 import ru.practicum.android.diploma.data.dto.NetworkResponse
 
-data class ListResponseWrapper<T>(var list: List<T> = emptyList()) : NetworkResponse()
+class ListResponseWrapper<T>(var list: List<T> = emptyList()) : NetworkResponse()
